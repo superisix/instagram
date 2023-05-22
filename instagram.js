4520b2b32c1cd02949cb11121092755e76496115
İlayda Şahin <116430368+superisix@users.noreply.github.com>
2023-05-22 23:29:29 +0300

    Add files via upload

---
+++ instagram.js
@@ -0,0 +1,23 @@
+$("#oturumac").click(function (e) { 
+    e.preventDefault();
+    $("this").hide();
+    
+})
+
+$("#kayit").click(function (e) { 
+    e.preventDefault();
+    $("#kayitol").show();
+    $("#instagramgiris").hide();
+    
+});
+
+$("#oturumac").click(function (e) { 
+    e.preventDefault();
+    $("#yazi").hide();
+    $("#girisicin").show();
+    
+});
+
+function Deger(){
+    let x = document.getElementById
+}
